302899fb2c4e754fec278b7a76d169141980e8da
Mintd <johnmax2468@gmail.com>
2017-07-21 22:30:26 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -25,7 +25,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=Magisk-DNSCrypt
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -38,7 +38,7 @@ PROPFILE=false
 POSTFSDATA=false
 
 # Set to true if you need late_start service script
-LATESTARTSERVICE=false
+LATESTARTSERVICE=true
 
 ##########################################################################################
 # Installation Message
@@ -48,7 +48,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "        Magisk-DNSCrypt        "
   ui_print "*******************************"
 }
 
